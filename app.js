66a6d512d50acb5bc59ee83d7f3e3625dcd7b67d
Worlixz <geremydumas@gmail.com>
2023-04-12 09:34:42 +0200

    1

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-import  Express  from 'express'
+const express = require('express')
+const multer = require('./js_serveur/multer-config')
+const path = require('path')
+const cors = require('cors')
+
+
 const PORT = 3000
 
-const app = Express()
+const app = express()
 
-app.use('/css', Express.static('css'))
-app.use('/js_client', Express.static('js_client'))
+app.use('/css', express.static('css'))
+app.use('/js_client', express.static('js_client'))
+app.use(cors())
+app.use('/images', express.static(path.join(__dirname, 'assets/img_articles')));
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index')
+})
+app.post('/img_articles',multer, (req, res) => {
+    console.log('test')
 })
 
 app.listen(PORT, function(){
